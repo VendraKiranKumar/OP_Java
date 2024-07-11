fe6c5814d0374d0393b99c4cc6d5591493a64333
VendraKiranKumar <vendrakirankumar05@gmail.com>
2024-07-11 16:09:01 +0530

    first commit

---
+++ AddTwoNumbers.java
@@ -0,0 +1,13 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        // Declare two variables to store the numbers
+        int num1 = 10; // You can change this value
+        int num2 = 20; // You can change this value
+
+        // Add the two numbers
+        int sum = num1 + num2;
+
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
